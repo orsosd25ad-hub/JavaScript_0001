2823f797fefb111ac366702745653c1e8b144fb8
orsosd25ad-hub <orsosd.25ad@acsjszki.hu>
2026-03-25 09:55:44 +0100

    save

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //alert("Hello World!");
 
 function Gomb() {
-    document.getElementById("szoveg").innerHTML = "Hello world"
+    document.getElementById("szoveg").innerHTML = "Good Boy"
+}
+
+function osszead() {
+    var szam1 = parseInt(document.getElementById("szam1").value);
+    var szam2 = parseFloat(document.getElementById("szam2").value);
+    var eredmeny = szam1 + szam2;
+    document.getElementById("eredmeny").innerHTML = eredmeny;
 }
